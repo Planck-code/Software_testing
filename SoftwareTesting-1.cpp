85209a91efe59d21aacc62438cdea4fce89beb9b
Planck <>
2025-03-25 10:58:06 +0800

    添加一个循环让程序可以连续测试多个用例

--- SoftwareTesting-1.cpp
+++ SoftwareTesting-1.cpp
@@ -3,48 +3,57 @@
 using namespace std;
 
 int main(){
-    int a,b,c,n;
+    char continueTest;
+    //添加一个循环让程序可以连续测试多个用例
+    do {
+        int a,b,c,n;
 
-    //输入三个大于0小于或等于100的整数
-    cout<<"输入三个大于0小于或等于100的整数：";
-    cin>>a>>b>>c;
+        //输入三个大于0小于或等于100的整数
+        cout<<"输入三个大于0小于或等于100的整数：";
+        cin>>a>>b>>c;
 
-    //检测abc
-    while(!(a>=1 && a<=100)){
-        cout<<"输入的a错误，请重新输入：";
-        cin>>a;
-    }
+        //检测abc
+        while(!(a>=1 && a<=100)){
+            cout<<"输入的a错误，请重新输入：";
+            cin>>a;
+        }
 
-    while(!(b>=1 && b<=100)){
-        cout<<"输入的b错误，请重新输入：";
-        cin>>b;
-    }
+        while(!(b>=1 && b<=100)){
+            cout<<"输入的b错误，请重新输入：";
+            cin>>b;
+        }
 
-    while(!(c>=1 && c<=100)){
-        cout<<"输入的c错误，请重新输入：";
-        cin>>c;
-    }
+        while(!(c>=1 && c<=100)){
+            cout<<"输入的c错误，请重新输入：";
+            cin>>c;
+        }
 
-    //检查构成三角形的类型
-    if((a+b>c) && (a+c>b) && (b+c>a)){
-        if(a==b && b==c){
-            n=1;}
-        else if(a==b || a==c || b==c){
-            n=2;}
-        else{
-            n=3;}
+        //检查构成三角形的类型
+        if((a+b>c) && (a+c>b) && (b+c>a)){
+            if(a==b && b==c){
+                n=1;}
+            else if(a==b || a==c || b==c){
+                n=2;}
+            else{
+                n=3;}
+            
+            if(n==1){
+                cout<<"构成等边三角形"<<endl;
+            }
+            else if(n==2){
+                cout<<"构成等腰三角形"<<endl;
+            }
+            else if(n==3){
+                cout<<"构成不等边三角形"<<endl;
+            }
+        }
+            else{
+                cout<<"不能构成三角形"<<endl;
+            }
+            
+            cout<<"\n是否继续测试？(y/n): ";
+            cin>>continueTest;
+        } while(continueTest == 'y');
         
-        if(n==1){
-            cout<<"构成等边三角形"<<endl;
-        }
-        else if(n==2){
-            cout<<"构成等腰三角形"<<endl;
-        }
-        else if(n==3){
-            cout<<"构成不等边三角形"<<endl;
-        }
+        return 0;
     }
-        else{
-            cout<<"不能构成三角形"<<endl;
-        }
-}
